3032133fca7319fbe2b5b3d5dd7ca84fa6ff37eb
RadoslawSzczepanski <radoslaw.szczepanski327@gmail.com>
2025-05-14 14:40:48 +0000

    6_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces 
 //Niepoprawny wiek – gdy jest mniejszy od 0 lub większy niż 100. Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 28-02-2023.
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class WrongStudentName extends Exception { }
@@ -30,8 +31,12 @@ class Main {
                     case 1: exercise1(); break;
                     case 2: exercise2(); break;
                     case 3: exercise3(); break;
-                    default: return;
+                    default:
+                        return;
                 }
+            } catch (InputMismatchException e) {
+                System.out.println("Wybór z menu musi być liczbą");
+                scan.nextLine();
             } catch (IOException e) {
                 System.out.println("Błąd wejścia/wyjścia!");
             } catch (WrongStudentName e) {
@@ -39,7 +44,7 @@ class Main {
             } catch (WrongStudentAge e) {
                 System.out.println("Wiek musi być liczbą z przedziału 1-99!");
             } catch (WrongStudentDate e) {
-                System.out.println("Błędna data urodzenia!");
+                System.out.println("Błędny format daty! Poprawny to DD-MM-YYYY.");
             }
         }
     }
@@ -49,7 +54,6 @@ class Main {
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
-        System.out.println("0 - aby wyjść z programu");
         return scan.nextInt();
     }
 
@@ -112,6 +116,8 @@ class Main {
         }
     }
 }
+
+
 
 
 
